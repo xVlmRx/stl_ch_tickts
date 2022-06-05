5cb4e99729c5dbf757b5d3b90d140ba772485ad6
xVlmRx <vribkinpostbox@gmail.com>
2022-06-05 22:41:54 +0300

    Test tables

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 import json
+from prettytable import PrettyTable
 
 url = 'https://api.selectel.ru/support/tickets'
-headers = {'X-Token': '$API_key',
+headers = {'X-Token': '7kUdYIQXjZfXt9APcikQyQKS9_20413',
            'Content-Type': 'application/json'}
 query_params = {"only_opened": "True", "page": "1"}
+table_ticket_id = PrettyTable()
+table_ticket_subject = PrettyTable()
+table_ticket_comments = PrettyTable()
+table_comments = PrettyTable()
 
 
 def get_from_api(ticket_id_get="no_tickets_id"):
@@ -22,8 +27,9 @@ def ticket_comments(ticket_id="no_tickets_id"):
 
 if __name__ == '__main__':
     for tickets in get_from_api().get('items', "Can't find key in dictionary"):
-        if tickets.get('is_client_author') == False and tickets.get('is_client_read') == False:
-            print("ID тикета:", tickets.get('number'), "\n", "Тема тикета:", tickets.get('summary'), "\n--- --- --- --- ---\nКомментарии к тикету:\n--- --- --- --- ---\n", ticket_comments(str(tickets.get('number'))))
-            print("*** *** *** *** ***")
-        else:
-            print("OK")
+        if tickets.get('is_client_author') == False and tickets.get('is_client_read') == True:
+            table_comments.field_names = ["Field name", "Data"]
+            table_comments.add_row(["ID тикета:", tickets.get('number')])
+            table_comments.add_row(["Тема тикета:", tickets.get('summary')])
+            table_comments.add_row(["Комментарии к тикету:", ticket_comments(str(tickets.get('number')))])
+            print(table_comments)
