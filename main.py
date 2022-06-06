cd778c2b97c611a64c69cc399cf393abebe2d471
xVlmRx <vribkinpostbox@gmail.com>
2022-06-06 22:11:33 +0300

    Output variants

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import requests
 import json
+from prettytable import PrettyTable
 
+api_key = "gBdBXfw2YirQ90kLy2kqtDhWF_20413"
 url = 'https://api.selectel.ru/support/tickets'
-headers = {'X-Token': '$API_key',
+headers = {'X-Token': api_key,
            'Content-Type': 'application/json'}
 query_params = {"only_opened": "True", "page": "1"}
+table_ticket_id = PrettyTable()
+table_ticket_subject = PrettyTable()
+table_ticket_comments = PrettyTable()
+table_comments = PrettyTable()
+tickets_list = []
 
 
 def get_from_api(ticket_id_get="no_tickets_id"):
     if ticket_id_get == "no_tickets_id":
         return requests.get(url, headers=headers, params=query_params).json()
     else:
-        return requests.get(url + "/" + ticket_id_get + "/comments", headers=headers).json()
+        return requests.get('{}/{}/comments'.format(url, ticket_id_get), headers=headers).json()
 
 
 def ticket_comments(ticket_id="no_tickets_id"):
@@ -22,8 +29,12 @@ def ticket_comments(ticket_id="no_tickets_id"):
 
 if __name__ == '__main__':
     for tickets in get_from_api().get('items', "Can't find key in dictionary"):
-        if tickets.get('is_client_author') == False and tickets.get('is_client_read') == False:
-            print("ID тикета:", tickets.get('number'), "\n", "Тема тикета:", tickets.get('summary'), "\n--- --- --- --- ---\nКомментарии к тикету:\n--- --- --- --- ---\n", ticket_comments(str(tickets.get('number'))))
-            print("*** *** *** *** ***")
-        else:
-            print("OK")
+        if tickets.get('is_client_author') == False and tickets.get('is_client_read') == True:
+            table_comments.field_names = ["Field name", "Data"]
+            table_comments.add_row(["ID тикета:", tickets.get('number')])
+            table_comments.add_row(["Тема тикета:", tickets.get('summary')])
+            table_comments.add_row(["Комментарии к тикету:", ticket_comments(str(tickets.get('number')))])
+            tickets_list.append("Subject:{}".format(tickets.get('summary')))
+            tickets_list.append("https://my.selectel.ru/tickets/conversation/{}".format(tickets.get('number')))
+    print('\n'.join(tickets_list))
+    print(table_comments)
